31e606f95cad3d8c379a53c1612f879ea48e056a
Ana Klimovic <ana.klimov@gmail.com>
2018-12-07 17:06:21 -0800

    script to create Pocket VPC with public and private subnets and NAT

---
+++ deploy/create-pocket-vpc.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+#******************************************************************************
+#    AWS VPC Creation Shell Script
+#******************************************************************************
+#
+# SYNOPSIS
+#    Automates the creation of a custom IPv4 VPC, having both a public and a
+#    private subnet, and a NAT gateway.
+#
+# DESCRIPTION
+#    This shell script leverages the AWS Command Line Interface (AWS CLI) to
+#    automatically create a custom VPC.  The script assumes the AWS CLI is
+#    installed and configured with the necessary security credentials.
+#
+#==============================================================================
+# Author: Joe Arauzo (joe@arauzo.net)
+#         https://github.com/kovarus/
+#         aws-cli-create-vpcs/blob/master/aws-cli-create-vpc.sh 
+#
+# Edits by: Ana Klimovic
+#==============================================================================
+#
+AWS_REGION="us-west-2"
+VPC_NAME="pocket-aws"
+VPC_CIDR="10.1.0.0/16"
+SUBNET_PUBLIC_CIDR="10.1.129.32/27"
+SUBNET_PRIVATE_CIDR="10.1.0.0/17"
+SUBNET_PUBLIC_AZ="us-west-2c"
+SUBNET_PRIVATE_AZ="us-west-2c"
+SUBNET_PUBLIC_NAME="pocket-kube-public"
+SUBNET_PRIVATE_NAME="pocket-kube-private"
+CHECK_FREQUENCY=5
+
+POCKET_VPC_NETWORK_CIDR=$VPC_CIDR
+POCKET_VPC_PRIVATE_NETWORK_CIDR=$SUBNET_PRIVATE_CIDR
+POCKET_VPC_PUBLIC_NETWORK_CIDR=$SUBNET_PUBLIC_CIDR
+POCKET_AWS_ZONE=$SUBNET_PRIVATE_AZ
+
+#==================================================
+
+
+# Create VPC
+echo "Creating VPC in preferred region..."
+VPC_ID=$(aws ec2 create-vpc \
+  --cidr-block $VPC_CIDR \
+  --query 'Vpc.{VpcId:VpcId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  VPC ID '$VPC_ID' CREATED in '$AWS_REGION' region."
+POCKET_VPC_ID=$VPC_ID
+
+# Add Name tag to VPC
+aws ec2 create-tags \
+  --resources $VPC_ID \
+  --tags "Key=Name,Value=$VPC_NAME" \
+  --region $AWS_REGION
+echo "  VPC ID '$VPC_ID' NAMED as '$VPC_NAME'."
+
+# Create Public Subnet
+echo "Creating Public Subnet..."
+SUBNET_PUBLIC_ID=$(aws ec2 create-subnet \
+  --vpc-id $VPC_ID \
+  --cidr-block $SUBNET_PUBLIC_CIDR \
+  --availability-zone $SUBNET_PUBLIC_AZ \
+  --query 'Subnet.{SubnetId:SubnetId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Subnet ID '$SUBNET_PUBLIC_ID' CREATED in '$SUBNET_PUBLIC_AZ'" \
+  "Availability Zone."
+POCKET_VPC_PUBLIC_SUBNET_ID=$SUBNET_PUBLIC_ID
+
+# Add Name tag to Public Subnet
+aws ec2 create-tags \
+  --resources $SUBNET_PUBLIC_ID \
+  --tags "Key=Name,Value=$SUBNET_PUBLIC_NAME" \
+  --region $AWS_REGION
+echo "  Subnet ID '$SUBNET_PUBLIC_ID' NAMED as" \
+  "'$SUBNET_PUBLIC_NAME'."
+
+# Create Private Subnet
+echo "Creating Private Subnet..."
+SUBNET_PRIVATE_ID=$(aws ec2 create-subnet \
+  --vpc-id $VPC_ID \
+  --cidr-block $SUBNET_PRIVATE_CIDR \
+  --availability-zone $SUBNET_PRIVATE_AZ \
+  --query 'Subnet.{SubnetId:SubnetId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Subnet ID '$SUBNET_PRIVATE_ID' CREATED in '$SUBNET_PRIVATE_AZ'" \
+  "Availability Zone."
+POCKET_VPC_PRIVATE_SUBNET_ID=$SUBNET_PRIVATE_ID
+
+# Add Name tag to Private Subnet
+aws ec2 create-tags \
+  --resources $SUBNET_PRIVATE_ID \
+  --tags "Key=Name,Value=$SUBNET_PRIVATE_NAME" \
+  --region $AWS_REGION
+echo "  Subnet ID '$SUBNET_PRIVATE_ID' NAMED as '$SUBNET_PRIVATE_NAME'."
+
+# Create Internet gateway
+echo "Creating Internet Gateway..."
+IGW_ID=$(aws ec2 create-internet-gateway \
+  --query 'InternetGateway.{InternetGatewayId:InternetGatewayId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Internet Gateway ID '$IGW_ID' CREATED."
+
+# Attach Internet gateway to your VPC
+aws ec2 attach-internet-gateway \
+  --vpc-id $VPC_ID \
+  --internet-gateway-id $IGW_ID \
+  --region $AWS_REGION
+echo "  Internet Gateway ID '$IGW_ID' ATTACHED to VPC ID '$VPC_ID'."
+
+# Create Route Table
+echo "Creating Route Table..."
+ROUTE_TABLE_ID=$(aws ec2 create-route-table \
+  --vpc-id $VPC_ID \
+  --query 'RouteTable.{RouteTableId:RouteTableId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Route Table ID '$ROUTE_TABLE_ID' CREATED."
+
+# Create route to Internet Gateway
+RESULT=$(aws ec2 create-route \
+  --route-table-id $ROUTE_TABLE_ID \
+  --destination-cidr-block 0.0.0.0/0 \
+  --gateway-id $IGW_ID \
+  --region $AWS_REGION)
+echo "  Route to '0.0.0.0/0' via Internet Gateway ID '$IGW_ID' ADDED to" \
+  "Route Table ID '$ROUTE_TABLE_ID'."
+
+# Associate Public Subnet with Route Table
+RESULT=$(aws ec2 associate-route-table  \
+  --subnet-id $SUBNET_PUBLIC_ID \
+  --route-table-id $ROUTE_TABLE_ID \
+  --region $AWS_REGION)
+echo "  Public Subnet ID '$SUBNET_PUBLIC_ID' ASSOCIATED with Route Table ID" \
+  "'$ROUTE_TABLE_ID'."
+
+# Enable Auto-assign Public IP on Public Subnet
+aws ec2 modify-subnet-attribute \
+  --subnet-id $SUBNET_PUBLIC_ID \
+  --map-public-ip-on-launch \
+  --region $AWS_REGION
+echo "  'Auto-assign Public IP' ENABLED on Public Subnet ID" \
+  "'$SUBNET_PUBLIC_ID'."
+
+# Allocate Elastic IP Address for NAT Gateway
+echo "Creating NAT Gateway..."
+EIP_ALLOC_ID=$(aws ec2 allocate-address \
+  --domain vpc \
+  --query '{AllocationId:AllocationId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Elastic IP address ID '$EIP_ALLOC_ID' ALLOCATED."
+
+# Create NAT Gateway
+NAT_GW_ID=$(aws ec2 create-nat-gateway \
+  --subnet-id $SUBNET_PUBLIC_ID \
+  --allocation-id $EIP_ALLOC_ID \
+  --query 'NatGateway.{NatGatewayId:NatGatewayId}' \
+  --output text \
+  --region $AWS_REGION)
+FORMATTED_MSG="Creating NAT Gateway ID '$NAT_GW_ID' and waiting for it to "
+FORMATTED_MSG+="become available.\n    Please BE PATIENT as this can take some "
+FORMATTED_MSG+="time to complete.\n    ......\n"
+printf "  $FORMATTED_MSG"
+FORMATTED_MSG="STATUS: %s  -  %02dh:%02dm:%02ds elapsed while waiting for NAT "
+FORMATTED_MSG+="Gateway to become available..."
+SECONDS=0
+LAST_CHECK=0
+STATE='PENDING'
+until [[ $STATE == 'AVAILABLE' ]]; do
+  INTERVAL=$SECONDS-$LAST_CHECK
+  if [[ $INTERVAL -ge $CHECK_FREQUENCY ]]; then
+    STATE=$(aws ec2 describe-nat-gateways \
+      --nat-gateway-ids $NAT_GW_ID \
+      --query 'NatGateways[*].{State:State}' \
+      --output text \
+      --region $AWS_REGION)
+    STATE=$(echo $STATE | tr '[:lower:]' '[:upper:]')
+    LAST_CHECK=$SECONDS
+  fi
+  SECS=$SECONDS
+  STATUS_MSG=$(printf "$FORMATTED_MSG" \
+    $STATE $(($SECS/3600)) $(($SECS%3600/60)) $(($SECS%60)))
+  printf "    $STATUS_MSG\033[0K\r"
+  sleep 1
+done
+printf "\n    ......\n  NAT Gateway ID '$NAT_GW_ID' is now AVAILABLE.\n"
+POCKET_NAT_ID=$NAT_GW_ID
+
+# Create route to NAT Gateway
+MAIN_ROUTE_TABLE_ID=$(aws ec2 describe-route-tables \
+  --filters Name=vpc-id,Values=$VPC_ID Name=association.main,Values=true \
+  --query 'RouteTables[*].{RouteTableId:RouteTableId}' \
+  --output text \
+  --region $AWS_REGION)
+echo "  Main Route Table ID is '$MAIN_ROUTE_TABLE_ID'."
+RESULT=$(aws ec2 create-route \
+  --route-table-id $MAIN_ROUTE_TABLE_ID \
+  --destination-cidr-block 0.0.0.0/0 \
+  --gateway-id $NAT_GW_ID \
+  --region $AWS_REGION)
+echo "  Route to '0.0.0.0/0' via NAT Gateway with ID '$NAT_GW_ID' ADDED to" \
+  "Route Table ID '$MAIN_ROUTE_TABLE_ID'."
+echo "COMPLETED"
--- deploy/env.sh
+++ deploy/env.sh
@@ -1,12 +1,17 @@
 export NAME=pocketcluster.k8s.local
 export KOPS_STATE_STORE=s3://my-kubernetes-statestore
 
-export POCKET_VPC_ID=vpc-xxxxxxxx
-export POCKET_VPC_PRIVATE_SUBNET_ID=subnet-xxxxxxxx
-export POCKET_VPC_PUBLIC_SUBNET_ID=subnet-xxxxxxxx
-export POCKET_VPC_NETWORK_CIDR=10.1.0.0/16
-export POCKET_VPC_PRIVATE_NETWORK_CIDR=10.1.0.0/17
-export POCKET_VPC_PUBLIC_NETWORK_CIDR=10.1.129.32/27
-export POCKET_NAT_ID=nat-xxxxxxxxxxxxxxxxx
-export POCKET_AWS_ZONE=us-west-2c
+# Note: If you use the create-pocket-vpc.sh script,
+#       the env vars below will be set already.
+#       If you create the VPC yourself, then
+#       uncomment and set these vars 
+
+#export POCKET_VPC_ID=vpc-xxxxxxxx
+#export POCKET_VPC_PRIVATE_SUBNET_ID=subnet-xxxxxxxx
+#export POCKET_VPC_PUBLIC_SUBNET_ID=subnet-xxxxxxxx
+#export POCKET_VPC_NETWORK_CIDR=10.1.0.0/16
+#export POCKET_VPC_PRIVATE_NETWORK_CIDR=10.1.0.0/17
+#export POCKET_VPC_PUBLIC_NETWORK_CIDR=10.1.129.32/27
+#export POCKET_NAT_ID=nat-xxxxxxxxxxxxxxxxx
+#export POCKET_AWS_ZONE=us-west-2c
 
